8c79674578cc0995ab3b6bf70c907b04168d2cf0
MTW-Coding <74625959+Noxudus@users.noreply.github.com>
2026-04-20 22:02:16 +0200

    noxtrellism4
    A Adafruit NeoTrellis M4 Step-Sequenzer

---
+++ noxtrellism4/noxtrellism4.ino
@@ -0,0 +1,71 @@
+#include <Adafruit_NeoPixel.h>
+#include <Adafruit_NeoPixel_ZeroDMA.h>
+#include <bittable.h>
+#include <pins.h>
+#include <Adafruit_ZeroDMA.h>
+#include <Adafruit_FlashCache.h>
+#include <Adafruit_FlashTransport.h>
+#include <Adafruit_SPIFlash.h>
+#include <Adafruit_SPIFlashBase.h>
+#include <flash_devices.h>
+#include <Adafruit_Keypad.h>
+#include <Adafruit_Keypad_Ringbuffer.h>
+#include <Adafruit_NeoTrellisM4.h>
+#include <FifteenStep.h>
+
+// ---------------------------------------------------------------------------
+//
+// noxtrellism4.ino
+//
+// A MIDI sequencer example using the Adafruit NeoTrellis M4
+// 1x NeoTrellis M4 Kit: https://www.adafruit.com/product/4020
+//
+// Required dependencies:
+// Adafruit NeoPixel
+// Adafruit DMA NeoPixel
+// Adafruit Zero DMA
+// Adafruit QSPI
+// Adafruit Keypad
+// Adafruit NeoTrellis M4
+//
+// Author: Noxudus
+// Copyright:
+// License:
+//
+// ---------------------------------------------------------------------------
+#include "FifteenStep.h"
+
+#define SEQUENCER_MEMORY 1024
+FifteenStep seq = FifteenStep(SEQUENCER_MEMORY);
+
+// set initial state for dynamic values
+int tempo = 134;
+int channel = 10;
+int pitch[] = { 36, 38, 42, 46 };  // general midi channel 10: kick, snare, hat closed, hat open
+int vel[] = { 100, 80, 80, 80 };
+int steps = 16;
+
+#include "config/ui.h"
+#include "config/trellis.h"
+
+#include "helpers/button.h"
+#include "helpers/callback.h"
+#include "helpers/display.h"
+#include "helpers/command.h"
+
+void setup() {
+
+  trellis_init();
+
+  // start sequencer and set callbacks
+  seq.begin(tempo, steps);
+  seq.setMidiHandler(midi);
+  seq.setStepHandler(step);
+
+  prevReadTime = millis();
+}
+
+void loop() {
+  seq.run();
+  readButtons();
+}
